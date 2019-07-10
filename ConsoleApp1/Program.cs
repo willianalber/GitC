a4c2f673a634df3471814212e7a0acfb491dcb4c
John Doe <john.doe@gmail.com>
2019-07-10 10:22:55 -0300

    exemplo de performace

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.Write("Hello Git");
-            _ = Console.ReadKey();
+            string[] nameList = new string[2] { "valor 1", "valor 2" };
+
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firstTime = DateTime.Now;
+
+            for(int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("yyyyMMddhhmmssfff");
+                Console.WriteLine(dateList[i]);
+                //Thread.Sleep(10);
+            }
+
+            Console.WriteLine((DateTime.Now - firstTime));
+
+           /* foreach (var item in dateList)
+                Console.WriteLine(item);*/
+
+            Console.ReadKey();
         }
     }
 }
