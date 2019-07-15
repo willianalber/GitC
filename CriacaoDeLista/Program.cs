4610f82f04ba7b0e83a595c33c9adfe1e95be931
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 08:41:14 -0300

    Forma facil de mostrar uma coleção de valores

---
+++ CriacaoDeLista/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriacaoDeLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lista = new string[10];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
+            }
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
