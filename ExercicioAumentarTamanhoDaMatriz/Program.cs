929e3dc05384d33fbf3e6e335b0e5d8abb1f641e
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 13:37:13 -0300

    comit

---
+++ ExercicioAumentarTamanhoDaMatriz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAumentarTamanhoDaMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] lista = new string[1, 3];
+
+            cadastrarValores(ref lista);
+            int op = 0;
+            while (op != 2)
+            {
+
+
+            }
+        }
+
+        private static void cadastrarValores(ref string[,] lista)
+        {
+            while (true)
+            {
+
+            }
+        }
+    }
+}
