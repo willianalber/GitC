a57e948d19461731349c722db366adb6724ad51f
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 10:07:23 -0300

    Commit SQL

---
+++ Query/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿insert into SacolaDaCooper
+(CaixaCafe,Leite,CervejaLong,CervejaLata,CervejaBarril)
+values
+(10,5,8,3,6);
+go
+select * from dbo.sacoladacooper
+
+update SacolaDaCooper set Acucar = 2 where Acucar is null
