8655175a19cf383a807868ec0907d62860e02549
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-06 16:40:04 -0300

    WPF

---
+++ WPFApp/WPFApp/View/ucLogin.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp.View
+{
+    /// <summary>
+    /// Interaction logic for ucLogin.xaml
+    /// </summary>
+    public partial class ucLogin : UserControl
+    {
+        public ucLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            teste(tbxLogin.Text,tbxSenha.Text);
+        }
+
+        public event EventHandler loginCorrect;
+        public void teste(string login, string senha)
+        {
+            if (login == "admin" && senha == "admin")
+            {
+                this.Visibility = Visibility.Hidden;
+                loginCorrect(null, new EventArgs());
+            }
+            else
+            {
+                MessageBox.Show("Login invalido");
+            }
+        }
+    }
+}
