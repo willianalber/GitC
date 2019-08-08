5d64cefcbb619779cc331a5fa940adec8632e1e1
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-08 09:10:46 -0300

    c

---
+++ WPF_Calculadora/WPF_Calculadora/View/ucCalculadora.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Calculadora.View
+{
+    /// <summary>
+    /// Interaction logic for ucCalculadora.xaml
+    /// </summary>
+    public partial class ucCalculadora : UserControl
+    {
+        public ucCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler EventoRetornaCaption;
+
+        public string retornaCapition(Button botao)
+        {
+            return botao.Content.ToString();
+        }
+        
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CalculadoraLEN.Class1 calcular = new CalculadoraLEN.Class1();
+
+            if (rbSomar.IsChecked == true)
+            {
+                lbResultado.Content = calcular.Soma_Lendaria(int.Parse(txtN1.Text), int.Parse(txtN2.Text)).ToString();
+            }else
+                if (rbSubtrair.IsChecked == true)
+            {
+                lbResultado.Content = calcular.Menos_Lendario(int.Parse(txtN1.Text), int.Parse(txtN2.Text)).ToString();
+            }
+            else
+                if (rbDividir.IsChecked == true)
+            {
+                lbResultado.Content = calcular.Divisao_Lendaria(int.Parse(txtN1.Text), int.Parse(txtN2.Text)).ToString();
+            }
+            else
+                if (rbMultiplicar.IsChecked == true)
+            {
+                lbResultado.Content = calcular.Multiplicacao_Lendaria(int.Parse(txtN1.Text), int.Parse(txtN2.Text)).ToString();
+            }
+
+        }
+
+        private void Bt1_Click(object sender, RoutedEventArgs e)
+        {
+            te
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WPF_Calculadora/WPF_Calculadora/View/ucLogin.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Calculadora.View
+{
+    /// <summary>
+    /// Interaction logic for ucLogin.xaml
+    /// </summary>
+    public partial class ucLogin : UserControl
+    {
+        public ucLogin()
+        {
+            InitializeComponent();
+
+        }
+
+        public event EventHandler LoginCorreto;
+
+        public void validaAcesso(string user, string password)
+        {
+            if (user == "admin" && password == "admin")
+            {
+                this.Visibility = Visibility.Hidden;
+                
+                LoginCorreto(null,new EventArgs());
+            }
+            else
+            {
+                MessageBox.Show("Senha ou usuario incorreto! verifique");
+            }
+        }
+
+        private void BtEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            validaAcesso(textUsuario.Text,passwdSenha.Password);
+        }
+    }
+}
