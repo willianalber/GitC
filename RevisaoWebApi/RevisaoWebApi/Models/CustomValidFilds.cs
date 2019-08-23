6d1462607dde6c0222d02ba65c71068301d2dd0b
Willian Carlos Alberto de Oliveira <willian.oliveira@hbsis.com.br>
2019-08-23 15:08:08 -0300

    commit

---
+++ RevisaoWebApi/RevisaoWebApi/Models/CustomValidFilds.cs
@@ -0,0 +1,62 @@
+﻿using RevisaoWebApi.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RevisaoWebApi.Models
+{
+    public class CustomValidFilds : ValidationAttribute
+    {
+        ContextDB db = new ContextDB();
+
+        private ValidFields TypeFild;
+
+        public CustomValidFilds(ValidFields type)
+        {
+            TypeFild = type;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                switch (TypeFild)
+                {
+                    case ValidFields.ValidaLogin:
+                        {
+                            Usuario user = db.usuarios.FirstOrDefault(x => x.login == value.ToString());
+
+                            if (user == null)
+                            {
+                                return ValidationResult.Success;
+                            }
+                            else
+                            {
+                                return new ValidationResult("Login já cadastrado em nossa base de dados.");
+                            }
+                        }break;
+                    case ValidFields.ValidaEmail:
+                        break;
+                    case ValidFields.ValidaSenha:
+                        break;
+                    case ValidFields.ValidaNome:
+                        {                           
+                                if (value.ToString().Contains("GIOMAR"))
+                                {
+                                    return ValidationResult.Success;
+                                }
+                                else
+                                {
+                                    return new ValidationResult("Muito triste com você.");
+                                }
+                        }break;
+                    default:
+                        break;
+                }
+            }
+            return new ValidationResult($"O campo {validationContext.DisplayName} é obrigatório.");
+        }
+    }
+}
---
+++ RevisaoWebApi/RevisaoWebApi/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RevisaoWebApi.Models
+{
+    public class Usuario : UserControl
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [CustomValidFilds(Enums.ValidFields.ValidaNome)]
+        public string Nome { get; set; }
+
+        [CustomValidFilds(Enums.ValidFields.ValidaEmail)]
+        public string Email { get; set; }
+
+        [CustomValidFilds(Enums.ValidFields.ValidaLogin)]
+        public string login { get; set; }
+
+        [CustomValidFilds(Enums.ValidFields.ValidaSenha)]
+        public string senha { get; set; }
+
+    }
+}
